c1a424a582bb112f8c2836323b4b932e031bf078
Raissa Villela <34253483+raissavillela@users.noreply.github.com>
2024-02-26 12:06:29 -0500

    Requisito 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Table from './Table';
+import { DataApi } from './types';
 
 function App() {
+  const [apiData, setApiData] = useState<DataApi[]>([]);
+
+  useEffect(() => {
+    fetch('https://swapi.dev/api/planets')
+      .then((response) => response.json())
+      .then((data) => setApiData(data.results));
+  }, []);
   return (
-    <span>Hello, App!</span>
+    <>
+      <h1>Star Wars</h1>
+      <Table planetInformation={ apiData } />
+    </>
   );
 }
 
